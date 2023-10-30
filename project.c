f1db864d2fcae76ec16b2c20f017acf71fc7ceaa
Maryam Islam <maryam.islam.232@northsouth.edu>
2023-10-30 21:02:37 +0600

    modified splash

--- project.c
+++ project.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "splash.h"
+#include <unistd.h>
+void splashscreen(void);
  int main(void)
 
  {
-    splashScreen();
-    Sleep(5000);
+    splashscreen();
+    sleep(5);
     system("cls");
-    Sleep(2000);
+    sleep(2);
 return 0; 
 }
 
