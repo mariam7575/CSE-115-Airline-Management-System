5a8d73f5618df44604293bb89d6f1865d46fdec4
Maryam Islam <maryam.islam.232@northsouth.edu>
2023-10-31 23:44:03 +0600

    login is created

---
+++ menu.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void MainMenu(void)
+{
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t*** Welcome to the Airline Reservation System ***\n\n");
+    printf("\t\t\t\t\t\t\t\t--------------------------------------------\n\n");
+    printf("\t\t\t\t\t\t\t\t1. Menu\n\n");
+    printf("\t\t\t\t\t\t\t\t2. Flight Schedule\n\n");
+    printf("\t\t\t\t\t\t\t\t3. Instruction\n\n");
+    printf("\t\t\t\t\t\t\t\t4. EXIT\n\n");
+    printf("\t\t\t\t\t\t\t\t--------------------------------------------\n\n");
+
+    printf("\t\t\t\t\t\t\t\tEnter Your Choice:     ");
+}
--- project.c
+++ project.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "splash.h"
 #include <unistd.h>
+#include "login.h"
 void splashscreen(void);
  int main(void)
 
@@ -10,6 +11,13 @@ void splashscreen(void);
     sleep(5);
     system("cls");
     sleep(2);
+    char username[50];
+ char password[50];
+printf("Enter required username:\n");
+gets(username);
+printf("Enter your password:\n");
+gets(password);
+username_password(username,password);
 return 0; 
 }
 
